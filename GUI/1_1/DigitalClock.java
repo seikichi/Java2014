a18729b9918cf766ccf0e3325e4b43e3f2e89834
Seiichi KONDO <seikichi@kmc.gr.jp>
2014-10-01 07:53:25 +0900

    adds digital clock jar

--- GUI/1_1/DigitalClock.java
+++ GUI/1_1/DigitalClock.java
@@ -2,46 +2,52 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
 public final class DigitalClock extends Frame implements Runnable {
+  private static int Width = 600;
+  private static int Height = 128;
+  private static int FontSize = 48;
+  private static int FontX = 32;
+  private static int FontY = 96;
+  private static String timeFormat = "yyyy/MM/dd HH:mm:ss";
+
   public DigitalClock() {
-    setBackground(Color.BLACK);
-    setFont(new Font("Osaka", Font.CENTER_BASELINE, 30));
     setResizable(false);
-    setSize(350, 55);
+    setSize(Width, Height);
     setTitle("Digital Clock");
     setVisible(true);
-
+    setFont(new Font(Font.DIALOG, Font.BOLD, FontSize));
     addWindowListener(new WindowAdapter() {
-      @Override public void windowClosing(WindowEvent event) {
-        System.exit(0);
-      }
+        @Override public void windowClosing(WindowEvent event) {
+          System.exit(0);
+        }
     });
   }
 
-  @Override
-  public final void paint(Graphics graphics) {
-    final Image image = createImage(this.getWidth(), this.getHeight());
-    final Graphics canvas = image.getGraphics();
-    canvas.setColor(Color.WHITE);
-    canvas.drawString(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new GregorianCalendar().getTime()), 10, 50);
-    graphics.drawImage(image, 0, 0, this);
+  @Override public final void paint(final Graphics g) {
+    final Graphics2D canvas = (Graphics2D) g;
+    final GregorianCalendar calender = new GregorianCalendar();
+    final SimpleDateFormat formatter = new SimpleDateFormat(timeFormat);
+    canvas.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                            RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+    canvas.setColor(Color.BLACK);
+    canvas.drawString(formatter.format(calender.getTime()), FontX, FontY);
   }
 
-  @Override
-  public final void run() {
+  @Override public final void run() {
     while (true) {
       repaint();
-
       try {
         Thread.sleep(1000);
       } catch (InterruptedException e){
-        return;
+        break;
       }
     }
   }
