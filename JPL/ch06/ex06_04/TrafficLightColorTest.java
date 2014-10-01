3db5e250d63e228f194e99e199bcb2dbb6cf3f29
Seiichi KONDO <seikichi@kmc.gr.jp>
2014-10-01 15:48:56 +0900

    adds test

---
+++ JPL/ch06/ex06_04/TrafficLightColorTest.java
@@ -0,0 +1,16 @@
+package ch06.ex06_04;
+
+import java.awt.Color;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.*;
+
+public class TrafficLightColorTest {
+  @Test
+  public void getColor() {
+    assertThat(TrafficLightColor.RED.getColor(), is(Color.RED));
+    assertThat(TrafficLightColor.YELLOW.getColor(), is(Color.YELLOW));
+    assertThat(TrafficLightColor.BLUE.getColor(), is(Color.BLUE));
+  }
+}
